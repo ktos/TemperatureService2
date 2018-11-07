6420d9893e811da7b465766d7f72b4109303dd5f
Marcin Badurowicz <m@badurowicz.net>
2018-11-07 22:28:10 +0100

    Add basic integrationtests

---
+++ TemperatureService2.Test/HomeController_IntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TemperatureService2.Test
+{
+    public class HomeController_Basic
+    : IClassFixture<WebApplicationFactory<TemperatureService2.Startup>>
+    {
+        private readonly WebApplicationFactory<TemperatureService2.Startup> _factory;
+
+        public HomeController_Basic(WebApplicationFactory<TemperatureService2.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Index")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
