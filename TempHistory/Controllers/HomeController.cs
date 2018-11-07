2dde9221cceac4ed1202b655139b32af42516767
Marcin Badurowicz <m@badurowicz.net>
2018-11-07 21:31:05 +0100

    initial revision

---
+++ TempHistory/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TempHistory.Models;
+
+namespace TempHistory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TempdataDbContext _context;
+
+        public HomeController(TempdataDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Tempdata.Where(x => x.Sensor == "outdoor").OrderByDescending(x => x.Timestamp).Take(100).ToList());            
+        }        
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TempHistory/Models/Tempdata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TempHistory.Models
+{
+    public partial class Tempdata
+    {
+        public string Sensor { get; set; }
+        public int Timestamp { get; set; }
+        public float Value { get; set; }
+    }
+}
