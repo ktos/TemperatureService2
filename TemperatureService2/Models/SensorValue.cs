7ca698e82c9de6ac48380ea9026a64c914777e8c
Marcin Badurowicz <m@badurowicz.net>
2018-11-12 17:29:03 +0100

    Add new model for a sensor and sensor value

---
+++ TemperatureService2/Models/SensorValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemperatureService2.Models
+{
+    /// <summary>
+    /// Value received from sensor
+    /// </summary>
+    public class SensorValue
+    {
+        /// <summary>
+        /// Sensor which sent the value
+        /// </summary>
+        public Sensor Sensor { get; set; }
+
+        /// <summary>
+        /// Value sent by the sensor
+        /// </summary>
+        public float Data { get; set; }
+
+        /// <summary>
+        /// Time the value was sent/received, as universal time
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+    }
+}
