71f72def918b4284f456d48c5617515f08cbf60d
Marcin Badurowicz <m@badurowicz.net>
2018-11-27 23:53:08 +0100

    Add showing errors also in Staging environment

--- TemperatureService3/Startup.cs
+++ TemperatureService3/Startup.cs
@@ -54,7 +54,7 @@ namespace TemperatureService3
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
