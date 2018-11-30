4dc97a04b3658b237441d49a156ac3d7a4f45ad9
Marcin Badurowicz <m@badurowicz.net>
2018-11-30 23:45:14 +0100

    Move menu into partial view

---
+++ TemperatureService3/ViewModels/MenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemperatureService3.ViewModels
+{
+    public class MenuViewModel
+    {
+        public IEnumerable<SensorViewModel> Sensors { get; set; }
+        public SensorViewModel Active { get; set; }
+    }
+}
